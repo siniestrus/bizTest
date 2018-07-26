78c3d998138b0a31d40f46a1b8697ec3b8581837
siniestrus <41081695+siniestrus@users.noreply.github.com>
2018-07-25 22:48:21 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 }
 app.use(bodyParser.json()); // support json encoded bodies
